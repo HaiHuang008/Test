940ebecc999087476e3a2ea6a90bba1cf6d9c52a
HaiHuang008 <113438936+HaiHuang008@users.noreply.github.com>
2022-10-12 11:27:35 +0800

    Create expect.sh

---
+++ expect.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/expect -f
+
+# spawn scp admin@ip:/home/admin/Young/*deb ./
+# expect "*password"
+# send "2019\n"
+# expect eof
+# sleep 5
+# spawn scp xxx.deb xxx.deb admin@ip:/home/admin
+# expect "*password"
+# send "YourPaSsWoRd\n"
+# expect eof
